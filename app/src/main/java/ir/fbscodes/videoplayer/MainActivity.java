23e2864216fa951f096d91dc347ce3fcbabbf583
saeid-hanafi <saeed.hanafi.ciw@gmail.com>
2022-10-30 18:25:21 +0330

    add-hls-video-support

--- app/src/main/java/ir/fbscodes/videoplayer/MainActivity.java
+++ app/src/main/java/ir/fbscodes/videoplayer/MainActivity.java
@@ -10,11 +10,15 @@ import android.widget.VideoView;
 
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.ui.StyledPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     private VideoView videoView;
+    private String hlsUrl = "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8";
     private String url = "https://hajifirouz10.cdn.asset.aparat.com/aparat-video/e45a22ee2397be5af3fcc5bd7dd969a448423121-360p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImFkYmU1M2JkYWFlMmFmYTJiODhkYTdlMDIxYmRmYzY2IiwiZXhwIjoxNjY3MTUwNjMwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.aBLlaJPQgR3mF8AkbdRPP_xzi627EIbCYwqDLbuq1J4";
 
     @Override
@@ -27,15 +31,28 @@ public class MainActivity extends AppCompatActivity {
         StyledPlayerView playerView = findViewById(R.id.exo_player_main);
         playerView.setPlayer(player);
 
-        // Build the media item.
-        MediaItem mediaItem = MediaItem.fromUri(Uri.parse(url));
-        // Set the media item to be played.
-        player.setMediaItem(mediaItem);
-        // Prepare the player.
-        player.prepare();
-        // Start the playback.
-        player.play();
+        // Without HLS
+//        // Build the media item.
+//        MediaItem mediaItem = MediaItem.fromUri(Uri.parse(url));
+//        // Set the media item to be played.
+//        player.setMediaItem(mediaItem);
+//        // Prepare the player.
+//        player.prepare();
+//        // Start the playback.
+//        player.play();
 
+        // By HLS
+        // Create a data source factory.
+        DataSource.Factory dataSourceFactory = new DefaultHttpDataSource.Factory();
+        // Create a HLS media source pointing to a playlist uri.
+                HlsMediaSource hlsMediaSource =
+                        new HlsMediaSource.Factory(dataSourceFactory)
+                                .createMediaSource(MediaItem.fromUri(Uri.parse(hlsUrl)));
+        // Set the media source to be played.
+                player.setMediaSource(hlsMediaSource);
+        // Prepare the player.
+                player.prepare();
+                player.play();
 
 
 
