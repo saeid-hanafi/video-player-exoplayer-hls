5836c633262610a6f522532627ce0a5d9cd47566
saeid-hanafi <saeed.hanafi.ciw@gmail.com>
2022-10-30 17:32:56 +0330

    use-exoplayer-library-for-video-player

--- app/src/main/java/ir/fbscodes/videoplayer/MainActivity.java
+++ app/src/main/java/ir/fbscodes/videoplayer/MainActivity.java
@@ -3,48 +3,71 @@ package ir.fbscodes.videoplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.ui.StyledPlayerView;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     private VideoView videoView;
+    private String url = "https://hajifirouz10.cdn.asset.aparat.com/aparat-video/e45a22ee2397be5af3fcc5bd7dd969a448423121-360p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImFkYmU1M2JkYWFlMmFmYTJiODhkYTdlMDIxYmRmYzY2IiwiZXhwIjoxNjY3MTUwNjMwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.aBLlaJPQgR3mF8AkbdRPP_xzi627EIbCYwqDLbuq1J4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        videoView = findViewById(R.id.video_player);
-        videoView.setVideoPath("https://hajifirouz10.cdn.asset.aparat.com/aparat-video/e45a22ee2397be5af3fcc5bd7dd969a448423121-360p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImFkYmU1M2JkYWFlMmFmYTJiODhkYTdlMDIxYmRmYzY2IiwiZXhwIjoxNjY3MTUwNjMwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.aBLlaJPQgR3mF8AkbdRPP_xzi627EIbCYwqDLbuq1J4");
-        videoView.start();
+        // Video Player With ExoPlayer Library
+        ExoPlayer player = new ExoPlayer.Builder(this).build();
+        StyledPlayerView playerView = findViewById(R.id.exo_player_main);
+        playerView.setPlayer(player);
 
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                MediaController mediaController = new MediaController(MainActivity.this);
-                mediaController.setMediaPlayer(videoView);
-                videoView.setMediaController(mediaController);
-                mediaController.setAnchorView(videoView);
-            }
-        });
+        // Build the media item.
+        MediaItem mediaItem = MediaItem.fromUri(Uri.parse(url));
+        // Set the media item to be played.
+        player.setMediaItem(mediaItem);
+        // Prepare the player.
+        player.prepare();
+        // Start the playback.
+        player.play();
 
-        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                videoView.start();
-            }
-        });
 
-        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
-            @Override
-            public boolean onError(MediaPlayer mp, int what, int extra) {
-                Snackbar.make(videoView, "Error: " + what + "Extra" + extra, Snackbar.LENGTH_LONG).show();
-                return false;
-            }
-        });
+
+
+        // Video Player With Android VideoView
+//        videoView = findViewById(R.id.video_player);
+//        videoView.setVideoPath(url);
+//        videoView.start();
+//
+//        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//            @Override
+//            public void onPrepared(MediaPlayer mp) {
+//                MediaController mediaController = new MediaController(MainActivity.this);
+//                mediaController.setMediaPlayer(videoView);
+//                videoView.setMediaController(mediaController);
+//                mediaController.setAnchorView(videoView);
+//            }
+//        });
+//
+//        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//            @Override
+//            public void onCompletion(MediaPlayer mp) {
+//                videoView.start();
+//            }
+//        });
+//
+//        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+//            @Override
+//            public boolean onError(MediaPlayer mp, int what, int extra) {
+//                Snackbar.make(videoView, "Error: " + what + "Extra" + extra, Snackbar.LENGTH_LONG).show();
+//                return false;
+//            }
+//        });
     }
 
     @Override
